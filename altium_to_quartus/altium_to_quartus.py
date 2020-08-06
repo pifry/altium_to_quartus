5b807f1d0f2d7cc66625d0f23debe7b8f1fcbf37
Magda <mauamagda@wp.pl>
2020-08-06 13:43:04 +0200

    implement args parser and input reader

--- altium_to_quartus/altium_to_quartus.py
+++ altium_to_quartus/altium_to_quartus.py
@@ -1,8 +1,27 @@
-import sys
+import argparse
 
-def parse_args(*args):
-    input_name = args[-1]
-    return (input_name, 0, 0)
+def func_name(input_file, u, output_file):
+    with open(input_file, "r") as reader, open(output_file, "w+") as writer:
+        for line in reader:
+            if line.startswith("NODENAME"):
+                lst = list(line.rstrip("\n").split(" "))
+                lst = list(filter(None, lst))
+                next_lst = next(iter(reader)).rstrip("\n").split(" ")
+                next_lst = list(filter(None, next_lst))
+                lst.extend(next_lst)
+                writer.write(str(" ".join(lst) + "\n"))
+
+
+def Main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", help="reference designator")
+    parser.add_argument("-o", "--output", help="output result to a file")
+    parser.add_argument("input_file", help="input file")
+
+    args = parser.parse_args()
+
+    result = func_name(args.input_file, args.u, args.output)
+    print(result)
 
 if __name__ == '__main__':
-    input_file_name, output_file_name, ref_des = parse_args(*sys.argv)
+    Main()
