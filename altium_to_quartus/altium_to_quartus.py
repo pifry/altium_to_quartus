5396e27e67dc0d2ff040dee0fc3f1afe77de41d1
Piotr Fryc <piotr.fryc@gmail.com>
2020-08-07 18:09:47 +0200

    Parameterize tests

--- altium_to_quartus/altium_to_quartus.py
+++ altium_to_quartus/altium_to_quartus.py
@@ -1,8 +1,12 @@
 import argparse
 
 def parse_arguments():
-    ...
-    return
+    class Ret:
+        def __init__(self):
+            self.input_file = ""
+            self.output = ""
+            self.u = ""
+    return Ret()
 
 if __name__ == '__main__':
     parse_arguments()
--- altium_to_quartus/tests/test_main.py
+++ altium_to_quartus/tests/test_main.py
@@ -1,49 +1,25 @@
 import sys
 import unittest
 from unittest.mock import patch
+from parameterized import parameterized
 from altium_to_quartus import parse_arguments
 
 class TestInputArgs(unittest.TestCase):
 
-    def test_full_input(self):
-        test_args = ['app_name', '-u', 'U3', '-o', 'output.qsv', 'input.net']
+    @parameterized.expand([
+        (['app_name', '-u', 'U3', '-o', 'output.qsv', 'input.net'], 'input.net', 'output.qsv', 'U3'),
+        (['app_name', '-o', 'output.qsv', '-u', 'U3', 'input.net'], 'input.net', 'output.qsv', 'U3'),
+        (['app_name', 'input.net'], 'input.net', 'input.qsv', 'U1'),
+        (['app_name', '-u', 'U3', 'input.net'], 'input.net', 'input.qsv', 'U3'),
+        (['app_name', '-o', 'output.qsv', 'input.net'], 'input.net', 'output.qsv', 'U1'),
+    ])
+    def test_args_parser(self, test_args, input_name, output_name, refdes):
         with patch.object(sys, 'argv', test_args):
             args = parse_arguments()
-            self.assertEquals(args.input_file,'input.net')
-            self.assertEquals(args.output,'output.qsv')
-            self.assertEquals(args.u,'U3')
+            self.assertEquals(args.input_file, input_name)
+            self.assertEquals(args.output, output_name)
+            self.assertEquals(args.u, refdes)
 
-    def test_full_input_reversed(self):
-        test_args = ['app_name', '-o', 'output.qsv', '-u', 'U3', 'input.net']
-        with patch.object(sys, 'argv', test_args):
-            args = parse_arguments()
-            self.assertEquals(args.input_file,'input.net')
-            self.assertEquals(args.output,'output.qsv')
-            self.assertEquals(args.u,'U3')
-    
-    def test_default_args(self):
-        test_args = ['app_name', 'input.net']
-        with patch.object(sys, 'argv', test_args):
-            args = parse_arguments()
-            self.assertEquals(args.input_file,'input.net')
-            self.assertEquals(args.output,'input.qsv')
-            self.assertEquals(args.u,'U1')
-
-    def test_specific_refdes(self):
-        test_args = ['app_name', '-u', 'U3', 'input.net']
-        with patch.object(sys, 'argv', test_args):
-            args = parse_arguments()
-            self.assertEquals(args.input_file,'input.net')
-            self.assertEquals(args.output,'input.qsv')
-            self.assertEquals(args.u,'U3')
-
-    def test_specific_output_name(self):
-        test_args = ['app_name', '-o', 'output.qsv', 'input.net']
-        with patch.object(sys, 'argv', test_args):
-            args = parse_arguments()
-            self.assertEquals(args.input_file,'input.net')
-            self.assertEquals(args.output,'output.qsv')
-            self.assertEquals(args.u,'U1')
 
 if __name__ == "__main__":
     unittest.main()
