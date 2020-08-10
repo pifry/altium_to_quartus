0446d6a410c69df60a6ddba58c5e5872aa353682
Magda <mauamagda@wp.pl>
2020-08-10 14:31:58 +0200

    working code

--- altium_to_quartus/altium_to_quartus.py
+++ altium_to_quartus/altium_to_quartus.py
@@ -1,15 +1,16 @@
+import sys
 import argparse
 from collections import defaultdict
 
 
-def func_name(input_file, u, output_file): 
+def quartus_file_generator(input_file, u, output_file):
     # transform input file into nested dicts
     dct = input_to_dict(input_file)
     # checking corresponding lines
     corresponding_line_checker(dct)
     # generating output file
     ref_des_writer(dct, u, output_file)
-    print("a kuku")
+    
 
 
 
@@ -18,7 +19,7 @@ def input_to_dict(input_file):
         big_dct = defaultdict(dict)
         for line in reader:
             small_dct = {}
-            
+
             if line.startswith("NODENAME"):
                 # clean one line
                 lst = list(line.rstrip("\n").rstrip("$").split(" "))
@@ -33,7 +34,7 @@ def input_to_dict(input_file):
                 #print(components_lst)
                 
                 if lst[1].endswith("_P") or lst[1].endswith("_N"):
-                    big_key = lst[1][:-2]
+                    big_key = lst[1][:-2].rstrip(".")
                     small_key = lst[1][-2:]
                     #print(big_key)
                     #print(small_key)
@@ -51,25 +52,32 @@ def input_to_dict(input_file):
 def corresponding_line_checker(dct):
     for net, small_dct in dct.items():
         if not "_" in small_dct.keys():
-            if not "_P" and not "_N" in item.keys():
+            if not "_P" in small_dct.keys() or not "_N" in small_dct.keys():
                 print(f"Net {net} belongs to a differential pair but has no corresponding negative or positive counterpart.")
-            return None
+                sys.exit(1)
 
 
 def ref_des_writer(dct, u, output_file):
-    print("my u is ", u)
     with open(output_file, "w+") as writer:
         for net, small_dct in dct.items():
             for item, comps in small_dct.items():
                 for (comp, pin) in comps:
                     if comp == u:
                         net_ending_digits = extract_net_ending_digits(net)
-                        if net_ending_digits != None:
-                            report = f"""set_location_assignment PIN_{pin} -to '{net} [{net_ending_digits}]'\n"""
+                        non_digit_net_name = extract_net_name(net)
+                        if "_" in net or "-" in net or "." in net:
+                            if net_ending_digits != None:
+                                if item == "_N":
+                                    report = report = f"""set_location_assignment PIN_{pin} -to '{non_digit_net_name}[{net_ending_digits}](n)'\n"""
+                                else:
+                                    report = f"""set_location_assignment PIN_{pin} -to '{non_digit_net_name}[{net_ending_digits}]'\n"""
+                            else:
+                                report = f"""set_location_assignment PIN_{pin} -to '{net}'\n"""
                         else:
                             report = f"""set_location_assignment PIN_{pin} -to '{net}'\n"""
                         writer.write(report)
             
+
 def extract_net_ending_digits(net_name):
     import re
     net_ending_regex = re.compile(r'(\d+)$')
@@ -80,43 +88,24 @@ def extract_net_ending_digits(net_name):
         return None
 
 
-        
-
-            # if not "_" in value.keys():
-            #     if "_P" and "_N" in value.keys():
-            #         print("ok")
-            #     else:
-            #         print(f"Net {key} belongs to a differential pair but has no corresponding negative or positive net.")
-            
-
-
-            # if "_P" in value.keys():
-            #     net_name = key
-            #     pin = value.values()[-1][-1]
-            #     report = f"""set_location_assignment PIN_{pin} -to '{net_name}'"""
-            #     print("report", report)
-                #print(net_name)
-                #report = f"""set_location_assignment PIN_{} -to '{}'"""
-                #writer.write(report, "\n")
-
-#with open(output_file, "w+") as writer
-
-# def create_corresponding_line(line):
-#     return line.split("_P$")[0]+"_N$"
-    
-
+def extract_net_name(net_name):
+    import re
+    if "_" in net_name or "-" in net_name or ".":
+        result = re.split(r'(\d+)$', net_name, 1)[0].rstrip("_-.")
+    else:
+        result = net_name
+    return result
 
 
 def Main():
     args = parse_arguments()
-    result = func_name(args.input_file, args.u, args.output)
-    print(result)
+    quartus_file_generator(args.input_file, args.u, args.output)
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", help="reference designator", default="U1")
-    parser.add_argument("-o", "--output", help="output result to a file", default="result.qsf")
+    parser.add_argument("-o", "--output", help="output result to a file", default = "result.qsf")
     parser.add_argument("input_file", help="input file")
 
     args = parser.parse_args()
