08871d4e03b12fb044d811a92fb1996868627a1c
Piotr Fryc <piotr.fryc@gmail.com>
2020-08-11 06:46:41 +0200

    Add test mechanism for main functionality

--- altium_to_quartus/altium_to_quartus.py
+++ altium_to_quartus/altium_to_quartus.py
@@ -8,5 +8,19 @@ def parse_arguments():
             self.u = ""
     return Ret()
 
+def a_to_q(input_data, ref_des):
+    content = list()
+    for line in input_data:
+        content.append(line)
+
+    for line in content:
+        yield line
+
 if __name__ == '__main__':
     parse_arguments()
+
+    input_data = ['pierwszy\n', 'drugi\n', 'trzeci\n']
+    with open('tmp.txt','w') as f:
+        for line in a_to_q(input_data, 'U3'):
+            f.write(line)
+    
